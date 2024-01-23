1d061f917a72172a902776b249d9ce562c1c2fcb
avinashp22 <avinashp22@gmail.com>
2024-01-23 21:37:20 +0000

    bestbuy-restassured-project-serenity

--- src/main/java/com/bestbuy/info/ProductSteps.java
+++ src/main/java/com/bestbuy/info/ProductSteps.java
@@ -7,16 +7,12 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import net.serenitybdd.annotations.Step;
 import net.serenitybdd.rest.SerenityRest;
-
 import java.util.HashMap;
-
 
 public class ProductSteps {
 
-    @Step("Create new product with name: {0}, type: {1}, price: {2}, upc: {3}, shipping: {4}, description: {5}," +
-            " manufacturer: {6}, model: {7}, image: {8}, url: {9}")
-    public ValidatableResponse createProduct(String name, String type, double price, String upc, int shipping,
-                                             String description, String manufacturer, String model, String image, String url) {
+    @Step("Create new product with name: {0}, type: {1}, price: {2}, upc: {3}, shipping: {4}, description: {5}, manufacturer: {6}, model: {7}, image: {8}, url: {9}")
+    public ValidatableResponse createProduct(String name, String type, double price, String upc, int shipping, String description, String manufacturer, String model, String image, String url) {
 
         ProductPojo productpojo = new ProductPojo();
         productpojo.setName(name);
@@ -48,10 +44,8 @@ public class ProductSteps {
                 .extract().path("");
     }
 
-    @Step("Update the product with Id: {0}, name: {1}, type: {2}, price: {3}, upc: {4}, shipping: {5}, description: {6}," +
-            "manufacturer: {7}, model: {8}, image: {9}, url: {10}")
-    public ValidatableResponse updateProduct(int productId, String name, String type, double price, String upc, int shipping,
-                                                 String description, String manufacturer, String model, String image, String url) {
+    @Step("Update the product with Id: {0}, name: {1}, type: {2}, price: {3}, upc: {4}, shipping: {5}, description: {6}, manufacturer: {7}, model: {8}, image: {9}, url: {10}")
+    public ValidatableResponse updateProduct(int productId, String name, String type, double price, String upc, int shipping, String description, String manufacturer, String model, String image, String url) {
         ProductPojo productPojo = new ProductPojo();
         productPojo.setName(name);
         productPojo.setType(type);
@@ -74,7 +68,6 @@ public class ProductSteps {
     }
 
     @Step("Delete the product with Id: {0}")
-
     public ValidatableResponse deleteProduct(int productId) {
         return SerenityRest.given().log().all()
                 .pathParam("productID", productId)
@@ -82,6 +75,5 @@ public class ProductSteps {
                 .delete(Path.PRODUCTS + EndPoints.DELETE_PRODUCT_BY_ID)
                 .then();
     }
-
 
 }
--- src/main/java/com/bestbuy/info/StoreSteps.java
+++ src/main/java/com/bestbuy/info/StoreSteps.java
@@ -7,17 +7,12 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import net.serenitybdd.annotations.Step;
 import net.serenitybdd.rest.SerenityRest;
-
 import java.util.HashMap;
-
 
 public class StoreSteps {
 
-    @Step("Create new Store with name: {0}, type: {1}, address: {2}, address2: {3}, city: {4}, " +
-            "state: {5}, zip: {6}, lat: {7}, lng: {8}, hours: {9}")
-
-    public ValidatableResponse createStore(String name, String type, String address, String address2, String city,
-                                           String state, String zip, Double lat, Double lng, String hours) {
+    @Step("Create new Store with name: {0}, type: {1}, address: {2}, address2: {3}, city: {4}, state: {5}, zip: {6}, lat: {7}, lng: {8}, hours: {9}")
+    public ValidatableResponse createStore(String name, String type, String address, String address2, String city, String state, String zip, Double lat, Double lng, String hours) {
 
         StorePojo storePojo = new StorePojo();
         storePojo.setName(name);
@@ -38,6 +33,7 @@ public class StoreSteps {
                 .post(EndPoints.CREATE_STORES)
                 .then().log().all();
     }
+
     @Step("Read Store with Id: {0}")
     public HashMap<String, Object> readStore(int storeId) {
         return SerenityRest.given().log().all()
@@ -47,9 +43,9 @@ public class StoreSteps {
                 .then().statusCode(200)
                 .extract().path("");
     }
+
     @Step("Update store with Id: {0}, name: {1}, type: {2}, address: {3}, address2: {4}, city: {5}, state: {6}, zip: {7}, lat: {8}, lng: {9}, hours: {10}")
-    public ValidatableResponse updateStore(int storeId, String name, String type, String address, String address2, String city,
-                                               String state, String zip, Double lat, Double lng, String hours) {
+    public ValidatableResponse updateStore(int storeId, String name, String type, String address, String address2, String city, String state, String zip, Double lat, Double lng, String hours) {
         StorePojo storePojo = new StorePojo();
         storePojo.setName(name);
         storePojo.setType(type);
@@ -70,8 +66,8 @@ public class StoreSteps {
                 .patch(Path.STORES + EndPoints.UPDATE_STORE_BY_ID)
                 .then().log().all();
     }
-    @Step("Delete Store with Id: {0}")
 
+    @Step("Delete Store with Id: {0}")
     public ValidatableResponse deleteStore(int storeId) {
         return SerenityRest.given().log().all()
                 .pathParam("storeID", storeId)
@@ -79,6 +75,4 @@ public class StoreSteps {
                 .delete(Path.STORES + EndPoints.DELETE_STORE_BY_ID)
                 .then();
     }
-
-
 }
